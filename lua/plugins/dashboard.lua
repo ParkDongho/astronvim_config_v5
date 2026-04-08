d52c9ef076f9c83fbfbc7a9fe84e217104403840
parkdongho <parkdongho28@naver.com>
2026-04-08 23:25:34 +0900

    update

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,40 +1,11 @@
--- You can also add or configure plugins by creating files in this `plugins/` folder
--- PLEASE REMOVE THE EXAMPLES YOU HAVE NO INTEREST IN BEFORE ENABLING THIS FILE
--- Here are some examples:
-
----@type LazySpec
 return {
-
-  -- == Examples of Adding Plugins ==
-
-  "andweeb/presence.nvim",
-  {
-    "ray-x/lsp_signature.nvim",
-    event = "BufRead",
-    config = function() require("lsp_signature").setup() end,
-  },
-
-  -- == Examples of Overriding Plugins ==
-
-  -- customize dashboard options
-  {
-    "folke/snacks.nvim",
-
-    init = function()
-        -- Then override snacks.nvim header color like so
-        local dashboard_blue = "#89B4FA"
-        local dashboard_white = "#F8F8F2"
-        vim.api.nvim_set_hl(0, "SnacksDashboardHeader", {
-            fg = dashboard_blue,
-            bold = true,
-        })
-    end,
-
-    opts = {
-      dashboard = {
-        preset = {
-          header = table.concat({
-                    "                    ⠀⠀⠀⠀⠀⠀⠀⠀⠀                                    ",
+    'nvimdev/dashboard-nvim',
+    event = 'VimEnter',
+    config = function()
+        require('dashboard').setup{
+            theme = 'doom',
+            config = {
+                header = {
                     "                    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣤⣤⣤⣤⣄⣀⣀⠀⠀⠀                     ",
                     "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⠶⣻⠝⠋⠠⠔⠛⠁⡀⠀⠈⢉⡙⠓⠶⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
                     "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠞⢋⣴⡮⠓⠋⠀⠀⢄⠀⠀⠉⠢⣄⠀⠈⠁⠀⡀⠙⢶⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
@@ -72,80 +43,39 @@ return {
                     "        ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║       ",
                     "        ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝       ",
                     "                                                                 "
-          }, "\n"),
-          keys = {
-            {
-              icon = '󰱼  ',
-              desc = 'Find File',
-              key = 'f',
-              action = ':Telescope find_files'
-            },
-            {
-                icon = '󰈚  ',
-                desc = 'Recent Files',
-                key = 'r',
-                action = ':Telescope oldfiles'
-            },
-            {
-                icon = '󰗼  ',
-                desc = 'Quit',
-                key = 'q',
-                action = ':qa'
+                },
+                center = {
+                    {
+                        icon = '󰱼  ',
+                        desc = 'Find File',
+                        key = 'f',
+                        action = 'Telescope find_files'
+                    },
+                    {
+                        icon = '󰈚  ',
+                        desc = 'Recent Files',
+                        key = 'r',
+                        action = 'Telescope oldfiles'
+                    },
+                    {
+                        icon = '󰗼  ',
+                        desc = 'Quit',
+                        key = 'q',
+                        action = 'qa'
+                    }
+                },
+                sections = {
+                  { section = "header", padding = 0 },     -- header 아래 여백 최소화
+                  { section = "keys",   gap = 1, padding = 1 },
+                  { section = "startup" },
+                },
+
             }
-          },
-        },
-
-        sections = {
-          { section = "header", padding = 0 },     -- header 아래 여백 최소화
-          { section = "keys",   gap = 1, padding = 1 },
-          { section = "startup" },
-        },
-
-      },
-    },
-  },
-
-  -- You can disable default plugins as follows:
-  { "max397574/better-escape.nvim", enabled = false },
-
-  -- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
-  {
-    "L3MON4D3/LuaSnip",
-    config = function(plugin, opts)
-      require "astronvim.plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
-      -- add more custom luasnip configuration such as filetype extend or custom snippets
-      local luasnip = require "luasnip"
-      luasnip.filetype_extend("javascript", { "javascriptreact" })
+        }
     end,
-  },
-
-  {
-    "windwp/nvim-autopairs",
-    config = function(plugin, opts)
-      require "astronvim.plugins.configs.nvim-autopairs"(plugin, opts) -- include the default astronvim config that calls the setup call
-      -- add more custom autopairs configuration such as custom rules
-      local npairs = require "nvim-autopairs"
-      local Rule = require "nvim-autopairs.rule"
-      local cond = require "nvim-autopairs.conds"
-      npairs.add_rules(
-        {
-          Rule("$", "$", { "tex", "latex" })
-            -- don't add a pair if the next character is %
-            :with_pair(cond.not_after_regex "%%")
-            -- don't add a pair if  the previous character is xxx
-            :with_pair(
-              cond.not_before_regex("xxx", 3)
-            )
-            -- don't move right when repeat character
-            :with_move(cond.none())
-            -- don't delete if the next character is xx
-            :with_del(cond.not_after_regex "xx")
-            -- disable adding a newline when you press <cr>
-            :with_cr(cond.none()),
-        },
-        -- disable for .vim files, but it work for another filetypes
-        Rule("a", "a", "-vim")
-      )
-    end,
-  },
+    dependencies = {
+        {'nvim-tree/nvim-web-devicons'},
+        {'nvim-telescope/telescope.nvim'},
+        {'nvim-lua/plenary.nvim'}
+    }
 }
